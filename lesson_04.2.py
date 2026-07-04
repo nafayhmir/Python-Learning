b1292ff55eebc27bda59ef1d60df61c90cb20415
Muhammad Nafayh Akhlaq <nafayhmir@gmail.com>
2026-07-04 21:40:41 +0500

    Learned While Loop

---
+++ lesson_04.2.py
@@ -0,0 +1,18 @@
+#Loops
+#While Loops
+
+#Basic Syntax while condition:
+#Printing Even Numbers
+'''x=0
+while x<=20:
+    if x%2==0:
+        print(x)
+    x+=1'''
+#Printing back numbers i.e 50->0
+
+number=int(input("Where should code start From? "))
+x=number
+while x>0:
+    print(x)
+    x=x-1
+    
